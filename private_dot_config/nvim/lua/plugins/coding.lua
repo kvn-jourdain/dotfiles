90e7781e963cf6c8f7d714ed384f201a33fd655b
Kévin JOURDAIN <kevin.jourdain62@gmail.com>
2024-07-15 22:16:19 +0200

    chore(nvim): fix nvim cmp

--- private_dot_config/nvim/lua/plugins/coding.lua
+++ private_dot_config/nvim/lua/plugins/coding.lua
@@ -71,12 +71,4 @@ return {
       position = "right",
     },
   },
-
-  {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
 }
